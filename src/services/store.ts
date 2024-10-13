d1de65a746c2cb9cfee8818fb2d6f6cf1fc71451
Aleksandr <q400w@mail.ru>
2024-10-13 12:24:48 +0400

    Ver 1.0

--- src/services/store.ts
+++ src/services/store.ts
@@ -1,23 +1,47 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { ThunkAction, ThunkDispatch, thunk } from 'redux-thunk';
 
 import {
   TypedUseSelectorHook,
   useDispatch as dispatchHook,
   useSelector as selectorHook
 } from 'react-redux';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 
-const rootReducer = () => {}; // Заменить на импорт настоящего редьюсера
+import burgerConstructor from './slices/burger-constructor/slice';
+import feed from './slices/feed/slice';
+import ingredients from './slices/ingredients/slice';
+import order from './slices/order/slice';
+import user from './slices/user/slice';
 
+// Объединяем все редюсеры в корневой редюсер
+export const rootReducer = combineReducers({
+  burgerConstructor,
+  feed,
+  ingredients,
+  order,
+  user
+});
+
+// Настройка хранилища с использованием Redux Toolkit
 const store = configureStore({
-  reducer: rootReducer,
-  devTools: process.env.NODE_ENV !== 'production'
+  reducer: rootReducer
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
 
-export type AppDispatch = typeof store.dispatch;
+type TApplicationActions = any;
 
-export const useDispatch: () => AppDispatch = () => dispatchHook();
+export type AppThunk<Return = void> = ThunkAction<
+  Return,
+  RootState,
+  unknown,
+  TApplicationActions
+>;
+
+export type AppDispatch = ThunkDispatch<RootState, never, TApplicationActions>;
+
+// Создаём типизированные хуки для использования в компонентах
+export const useDispatch = () => dispatchHook<AppDispatch>();
 export const useSelector: TypedUseSelectorHook<RootState> = selectorHook;
 
 export default store;
